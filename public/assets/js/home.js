74b4c4f1d9bf120481354f65e77d7b536e6a192c
Jeremy Oglesby <iaminaband@gmail.com>
2019-09-28 11:33:03 -0700

    Mid Lesson 16 - issue with http post, email  empty

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,6 +1,9 @@
 const $setLogin = $('#login');
 const $setSignUp = $('#sign-up');
 const $submitButton = $('#submit');
+const $emailInput = $('#email');
+const $passwordInput = $('#password');
+const $message = $('#message');
 
 let authSetting = 'login';
 
@@ -18,5 +21,26 @@ function setAuth(setting) {
   }
 }
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
+
+  if (!email || !password) {
+    displayMessage('Please supply a valid email and password.', 'danger');
+    return;
+  }
+
+  console.log(
+    `Email: ${email} || Password: ${password} || Auth Settings: ${authSetting}`
+  );
+}
+
+function displayMessage(message, type) {
+  $message.text(message).attr('class', type);
+}
+
 $setLogin.on('click', setAuth.bind(null, 'login'));
 $setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handleFormSubmit);
